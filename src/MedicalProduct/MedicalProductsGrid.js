8aa74008acd5872dc7d4dee25f379cba9d91eeb4
bermoodastar <bermooda.star@gmail.com>
2019-09-01 19:23:18 +0430

    fixed medical product grid (was mistakenly replaced with edit)

--- src/MedicalProduct/MedicalProductsGrid.js
+++ src/MedicalProduct/MedicalProductsGrid.js
@@ -1,239 +1,246 @@
 import React , {Component} from 'react';
-import {Button, FormGroup, ControlGroup, TextArea} from '@blueprintjs/core';
-import '../Css/netdrug.css';
-import { Redirect } from 'react-router-dom';
+import { ColumnDirective, ColumnsDirective, Filter, GridComponent, Group, Inject, Page, Sort } from '@syncfusion/ej2-react-grids';
 import * as ConstantValues from '../Constants';
-//import { getBase64 } from '../Utilities/FileHelper';
+import '../../node_modules/@syncfusion/ej2-base/styles/bootstrap.css';  
+import '../../node_modules/@syncfusion/ej2-buttons/styles/bootstrap.css';  
+import '../../node_modules/@syncfusion/ej2-calendars/styles/bootstrap.css';  
+import '../../node_modules/@syncfusion/ej2-dropdowns/styles/bootstrap.css';  
+import '../../node_modules/@syncfusion/ej2-inputs/styles/bootstrap.css';  
+import '../../node_modules/@syncfusion/ej2-navigations/styles/bootstrap.css';
+import '../../node_modules/@syncfusion/ej2-popups/styles/bootstrap.css';
+import '../../node_modules/@syncfusion/ej2-splitbuttons/styles/bootstrap.css';
+import "../../node_modules/@syncfusion/ej2-react-grids/styles/bootstrap.css";
+import {Button} from '@blueprintjs/core';
+import { Redirect } from 'react-router-dom';
 import { MedicalProduct } from '../Models/MedicalProduct';
 import AuthenticationService from '../AuthenticationService';
 
-class MedicalProductEdit extends Component
+class MedicalProductsGrid extends Component
 {
-    constructor(args)
+    constructor(props)
     {
-        super(args);
-       
-        this.OnFormDataChanged=this.OnFormDataChanged.bind(this);
-
-        this.OnSaveButtonClicked = this.OnSaveButtonClicked.bind(this);
-        this.OnCancelButtonClicked = this.OnCancelButtonClicked.bind(this);
-        //this.OnImageSelected = this.OnImageSelected.bind(this);
+        super(props);
+        this.SelectedId = '';
+        this.OnRowSelected = this.OnRowSelected.bind(this);
+        this.OnAddButtonClicked = this.OnAddButtonClicked.bind(this);
+        this.OnEditButtonClicked = this.OnEditButtonClicked.bind(this);
+        this.OnRowDeselected = this.OnRowDeselected.bind(this);
+        this.OnDetailsButtonClicked = this.OnDetailsButtonClicked.bind(this);
+        this.OnNextPageClicked = this.OnNextPageClicked.bind(this);
+        this.OnPreviousPageClicked = this.OnPreviousPageClicked.bind(this);
         this.state = {
-                Data: new MedicalProduct(),
-                IsCanceled : false,
-                IsLoading : false,
-                SavedSuccessfuly : false
-            }
-       
+            Data: [{
+                LatinName : '',
+                PersianName : '',
+                ImageUrl : '',
+                Image360Url : '',
+                ProductType : '',
+                Size : '',
+                Producer : '',
+                Website : '',
+                ConsumingZone : '',
+                AgeLimit : '',
+                GenderLimit : '',
+                CountInPackage : '',
+                MedicalCode : '',
+                Group : '',
+                PackageType : '',
+                Barcode : '',
+                Category : '',
+                Description : '',
+                Specifications : '',
+                Usecases : '',
+                HowToUse : '',
+                HowToKeep : '',
+                Combinations : '',
+                Price : '',
+                Color : '',
+                Material : '',
+                Code : '',
+                Id : '',
+            }],
+            IsLoading : true,
+            AddNewDrugClicked : false,
+            EditDrugClicked : false,
+            CanEdit : false,
+            DetailsButtonClicked : false,
+            LoadingFailed : false,
+            CurrentPage : 0,
+            TotalRows : 0,
+
+        };
+        if(this.state.IsLoading)
+        {
+            this.LoadMedicalProducts(1,50);
+        }
         
     }
-    componentDidMount()
+    
+
+    render(){
+        console.log("rendering");
+        if(this.state.AddNewDrugClicked)
+        {
+            return (<Redirect to="/medicalproduct/new"/>);
+        }
+        if(this.state.EditDrugClicked)
+        {
+            let url = `/medicalproduct/edit/${this.SelectedId}`;
+            return (<Redirect to={url}/>);
+        }
+        if(this.state.IsLoading)
+        {
+            return (<span>Loading...</span>);
+        }
+        if(this.state.DetailsButtonClicked)
+        {
+            let url = `/medicalproduct/details/${this.SelectedId}`;
+            return (<Redirect to={url}/>);
+        }
+        if(this.state.LoadingFailed)
+        {
+            return (<span>Error while loading data!</span>);
+        }
+        return (
+       <div>
+            <div className="ml-5 mr-5 mt-2">
+                <Button text="Add" className="mr-2" onClick={this.OnAddButtonClicked}/>
+                <Button text="Edit" className="mr-2" onClick={this.OnEditButtonClicked} />
+                <Button text="Details" className="mr-2" onClick={this.OnDetailsButtonClicked} />
+            </div>        
+            <div className="mt-2 mb-5 mr-5 ml-5">
+                <div>
+                    <span className="mr-2">Page {this.state.CurrentPage} of {Math.ceil(this.state.TotalRows/50)}</span>
+                    <Button text="Previous" onClick={this.OnPreviousPageClicked} className="mr-2"/>
+                    <Button text="Next" onClick={this.OnNextPageClicked} className="mr-2"/>
+                </div>
+                <GridComponent dataSource={this.state.Data} allowPaging={true} /*pageSettings={{pageSize : 20}}
+                allowSorting={true} allowFiltering={true}*/ rowSelected={this.OnRowSelected} rowDeselected={this.OnRowDeselected}>
+                    <ColumnsDirective>
+                        <ColumnDirective field='Id' visible={false}/>
+                        <ColumnDirective field='LatinName' headerText="Latin Name" width='100' />
+                        <ColumnDirective field='PersianName' headerText="Persian Name" width='100'/>
+                    </ColumnsDirective>
+                    {/* <Inject services={[Page, Sort, Filter, Group]} /> */}
+                
+                 </GridComponent>
+            </div>
+        </div>
+            );
+    }
+
+    LoadMedicalProducts(pageNumber, rowsInPage)
     {
-        const { match : {params}} = this.props;
-        this.setState({
-            IsLoading : true
-        });
         let thisObject = this;
-        
-        fetch(`${ConstantValues.WebApiBaseUrl}/${ConstantValues.MedicalProductGetApi}/${params.productId}`,
+        fetch(`${ConstantValues.WebApiBaseUrl}/${ConstantValues.MedicalProductGetPagedApi}?pagenumber=${pageNumber}&rowsinpage=${rowsInPage}`,
         {
             method : "GET",
             headers:{
                'Content-Type' : 'text/plain; charset=utf-8',
                'Authorization' : `Bearer ${AuthenticationService.GetAuthToken()}`
             }
-            }).then(response => {
-               return response.text();
+        }).then(function(response){
+                //console.log(response.text());
+                let temp = response.text();
              
-               }
+                return temp;}
             ).then(responseText =>{
                 try{
-                    return JSON.parse(responseText);
+                    //console.log(responseText);
+                    const responseJson = JSON.parse(responseText);
+                    return responseJson;
                 }
                 catch(e)
                 {
                     Promise.reject({exception:e, body: responseText, type:'unparsable'});
+                    console.log("error on load");
+                    thisObject.setState({
+                        LoadingFailed : true
+                    });
                 }
             }).then(data=>{
-                
                 thisObject.setState(
                     {
-                        Data: data.Data,
+                        Data: data.Data.CurrentPageData,
+                        CurrentPage : data.Data.CurrentPage,
+                        TotalRows : data.Data.TotalRows,
                         IsLoading : false,
-                        SavedSuccessfuly : false
+                        LoadingFailed : false
                     }
                 );
+                //console.log(data);
+            }).catch(e=>{
+                console.log(e);
+                thisObject.setState({
+                    LoadingFailed : true,
+                    IsLoading : false
+                });
             });
-    }
-    render(){
-        if(this.state.IsCanceled || this.state.SavedSuccessfuly)
-        {
-            return <Redirect to="/medicalproduct/list"/>
-        }
-        
-        return (
-            
-            <div className="m-5">
-                <ControlGroup vertical={true}>
-                    <FormGroup label="Latin Name:" labelFor="text-input">
-                        <input type="text" className="col-8" name="LatinName" value={this.state.Data.LatinName} onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    <FormGroup label="Persian Name:" labelFor="text-input">
-                        <input type="text" className="col-8" name="PersianName" value={this.state.Data.PersianName} onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    <FormGroup label="Product Type:" labelFor="text-input">
-                        <input type="text" className="col-8" name="ProductType" value={this.state.Data.ProductType} onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    <FormGroup label="Size:" labelFor="text-input">
-                        <input type="text" className="col-8" name="Size" value={this.state.Data.Size} onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    <FormGroup label="Producer:" labelFor="text-input">
-                        <input type="text" className="col-8" name="Producer" value={this.state.Data.Producer} onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    <FormGroup label="Website:" labelFor="text-input">
-                        <input type="text" className="col-8" name="Website" value={this.state.Data.Website} onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    <FormGroup label="Consuming Zone:" labelFor="text-input">
-                        <TextArea className="col-8" growVertically={true} name="ConsumingZone" value={this.state.Data.ConsumingZone} onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    <FormGroup label="Age Limit:" labelFor="text-input">
-                        <input type="text" className="col-8" name="AgeLimit" value={this.state.Data.AgeLimit} onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    <FormGroup label="Gender Limit:" labelFor="text-input">
-                        <input type="text" className="col-8" name="GenderLimit" value={this.state.Data.GenderLimit} onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    <FormGroup label="Counts In Package:" labelFor="text-input">
-                        <input type="text" className="col-8" name="CountsInPackage" value={this.state.Data.CountInPackage} onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    <FormGroup label="Medical Code:" labelFor="text-input">
-                        <input type="text" className="col-8" name="MedicalCode" value={this.state.Data.MedicalCode} onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    <FormGroup label="Group:" labelFor="text-input">
-                        <input type="text" className="col-8" name="Group" value={this.state.Data.Group} onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    <FormGroup label="Package Type:" labelFor="text-input">
-                        <input type="text" className="col-8" name="PackageType" value={this.state.Data.PackageType} onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    <FormGroup label="Barcode:" labelFor="text-input">
-                        <input type="text" className="col-8" name="Barcode" value={this.state.Data.Barcode}  onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    <FormGroup label="Category:" labelFor="text-input">
-                        <input type="text" className="col-8" name="Category" value={this.state.Data.Category} onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    <FormGroup label="Description:" labelFor="text-input">
-                        <TextArea className="col-8" growVertically={true} name="Description" value={this.state.Data.Description} onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    <FormGroup label="Specifications:" labelFor="Specifications">
-                        <TextArea className="col-8" growVertically={true} name="Specifications" value={this.state.Data.Specifications} onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    <FormGroup  label="Usecases:" labelFor="text-input">
-                        <TextArea className="col-8" growVertically={true} name="Usecases" value={this.state.Data.Usecases} onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    <FormGroup label="How To Use:" labelFor="text-input">
-                        <TextArea className="col-8" growVertically={true} name="HowToUse" value={this.state.Data.HowToUse} onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    <FormGroup label="How To Keep:" labelFor="text-input">
-                        <TextArea className="col-8" growVertically={true} name="HowToKeep" value={this.state.Data.HowToKeep} onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    <FormGroup label="Combinations:" labelFor="Combinations">
-                        <TextArea className="col-8" growVertically={true} name="Combinations" value={this.state.Data.Combinations} onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    <FormGroup label="Price:" labelFor="text-input">
-                        <input type="text" className="col-8" name="Price" value={this.state.Data.Price} onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    <FormGroup label="Color:" labelFor="text-input">
-                        <input type="text" className="col-8" name="Color" value={this.state.Data.Color} onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    <FormGroup label="Material:" labelFor="text-input">
-                        <input type="text" className="col-8" name="Material" value={this.state.Data.Material} onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    <FormGroup label="Code:" labelFor="text-input">
-                        <input type="text" className="col-8" name="Code" value={this.state.Data.Code} onChange={this.OnFormDataChanged}/>
-                    </FormGroup>
-                    {/* <FormGroup label="Image:" labelFor="text-input">
-                        <FileInput text="Choose Image..." onInputChange={this.OnImageSelected}/>
-                    </FormGroup> */}
-                    <div>
-                        <Button className="mr-2" intent="primary" text="Save" onClick={this.OnSaveButtonClicked}/>
-                        <Button className="ml-2" intent="none" text="Cancel" onClick={this.OnCancelButtonClicked}/>
-                    </div>
-                </ControlGroup>
-            </div>
-        );
-    }
-
-    OnFormDataChanged(event)
-    {
-        let eventTarget = event.currentTarget;
-        this.setState(lastState =>(
-            {
-                Data:{
-                    ...lastState.Data,
-                    [eventTarget.name] : eventTarget.value
-                }
-            })
-        );
-    }
-
-  
-    
-    OnCancelButtonClicked(event)
-    {
-        this.setState({
-            IsCanceled : true
-        });
-    }
-
-    OnSaveButtonClicked(event)
-    {
-         //console.log(this.state);
-        this.setState({
-            IsLoading : true
-        });
-        let thisObject = this;
-        fetch(`${ConstantValues.WebApiBaseUrl}/${ConstantValues.MedicalProductUpdateApi}`,
-        {
-            method : 'POST',
-            headers: {
-                'Content-Type' : 'application/json'
-            },
-            body: JSON.stringify(this.state.Data)
-        }).then(response => {
-            return response.text();
-        }).then(responseText => {
-            try{
-                return JSON.parse(responseText);
-            }
-            catch(e)
-            {
-                Promise.reject({exception:e, body: responseText, type:'unparsable'})
-            }
-        }).then(data=>{
-            console.log(data);
-            thisObject.setState({
-                IsLoading : false,
-                SavedSuccessfuly : true
-            });
-        });
-    }
-
-    // OnImageSelected(event)
-    // {
-    //     let target = event.currentTarget;
-    //     let selectedFile = target.files[0];
-    //     getBase64(selectedFile).then(base64=>{
-    //         this.setState(lastState=>(
-    //             {
-    //                 Data:{
-    //                     ...lastState.Data,
-    //                     ImageData : base64,
-    //                     ImageFileName : selectedFile.name
-    //                 }
-    //             }
-    //         ));
-    //     });
+                // response.json().then(data => {
+                //     if(response.status == 200)
+                //     {
+                //         thisObject.setState({
+                //             Data : data,
+                //             IsLoading : false
+                //         });                      
+                //     }
+                //     console.log(data);
+                // });
+                
+           
           
-    // }
+        
+    }
+    OnRowSelected(event)
+    {
+        this.SelectedId = event.data.Id;
+        // this.setState({
+        //     CanEdit : true
+        // });
+    }
+    OnAddButtonClicked(event)
+    {
+        console.log(this.SelectedId);
+        this.setState({
+            AddNewDrugClicked : true
+        });
+    }
+    OnEditButtonClicked(event)
+    {
+        //console.log(this.SelectedId);
+        this.setState({
+            EditDrugClicked : true
+        });
+    }
+    OnRowDeselected(event)
+    {
+        this.SelectedId = '';
+        // this.setState({
+        //     CanEdit : false
+        // });
+    }
+    OnDetailsButtonClicked(event)
+    {
+        this.setState({
+            DetailsButtonClicked : true
+        });
+    }
+    OnNextPageClicked(event)
+    {
+        let nextPage = this.state.CurrentPage + 1;
+        if(nextPage > Math.ceil(this.state.TotalRows / 50))
+        {
+            return;
+        }
+        this.LoadMedicalProducts(nextPage,50);
+    }
+    OnPreviousPageClicked(event)
+    {
+        let prevPage = this.state.CurrentPage - 1;
+        if(prevPage < 1)
+            return;
+        this.LoadMedicalProducts(prevPage,50);
+    }
 }
 
-export default MedicalProductEdit;
+export default MedicalProductsGrid;
